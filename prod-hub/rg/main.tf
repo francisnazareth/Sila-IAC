81648461d1856356f4467afa18e82f48a3041c58
francisnazareth <simy@outlook.com>
2024-07-08 07:10:05 +0300

    typos

--- prod-hub/rg/main.tf
+++ prod-hub/rg/main.tf
@@ -1,24 +1,24 @@
 resource azurerm_resource_group rg_backup    {
   location     = var.location
-  name = "rg-backup-{var.customerprefix}-{var.environment}-{var.regionprefix}-01"
+  name = "rg-backup-${var.customerprefix}-${var.environment}-${var.regionprefix}-01"
   tags     = var.tags
 }
 
 resource azurerm_resource_group rg_monitoring    {
   location     = var.location
-  name = "rg-monitoring-{var.customerprefix}-{var.environment}-{var.regionprefix}-01"
+  name = "rg-monitoring-${var.customerprefix}-${var.environment}-${var.regionprefix}-01"
   tags     = var.tags
 }
 
 resource azurerm_resource_group rg_management    {
   location     = var.location
-  name = "rg-management-{var.customerprefix}-{var.environment}-{var.regionprefix}-01"
+  name = "rg-management-${var.customerprefix}-${var.environment}-${var.regionprefix}-01"
   tags     = var.tags
 }
 
 resource azurerm_resource_group rg_network    {
   location     = var.location
-  name = "rg-network-{var.customerprefix}-{var.environment}-{var.regionprefix}-01"
+  name = "rg-network-${var.customerprefix}-${var.environment}-${var.regionprefix}-01"
   tags     = var.tags
 }
 
