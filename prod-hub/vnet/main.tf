4456548db3a7de86911a53b3c55f748dd8fe710b
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-16 23:30:47 +0300

    Update main.tf

--- prod-hub/vnet/main.tf
+++ prod-hub/vnet/main.tf
@@ -55,3 +55,31 @@ resource "azurerm_subnet" "hub-sharedsvc-subnet-01" {
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
   address_prefixes     = [var.shared-svcs-snet-address-space]
 }
+
+output "vnetgw-snet-id" {
+   value = azurerm_subnet.hub-gateway-subnet.id
+}
+
+output "bastion-snet-id" {
+  value = azurerm_subnet.hub-bastion-subnet.id
+}
+
+output "firewall-snet-id" {
+   value = azurerm_subnet.hub-firewall-subnet.id
+}
+
+output "appgw-snet-id" {
+   value = azurerm_subnet.hub-appgw-subnet.id
+}
+
+output "management-snet-1-id" {
+  value = azurerm_subnet.hub-mgmt-subnet-01.id
+}
+
+output "pe-snet-id" {
+  value = azurerm_subnet.hub-pe-subnet.id
+}
+
+output "sharedsvc-snet-id" {
+  value = azurerm_subnet.hub-sharedsvc-subnet-01.id
+}
