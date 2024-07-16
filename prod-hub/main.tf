bc6a2646788cf9d02ddbdab8baef8a171e6a891c
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-16 22:40:14 +0300

    Update main.tf

--- prod-hub/main.tf
+++ prod-hub/main.tf
@@ -17,3 +17,17 @@ module "log-analytics-workspace" {
     law-rg-name = module.rg.monitoring_rg_name
 }
 
+module "hub-vnet" {
+    source               = "./vnet"
+    rg-name              = module.rg.rg-name
+    rg-location          = var.location
+    hub-vnet-address-space           = var.hub-vnet-address-space
+    firewall-subnet-address-space    = var.firewall-subnet-address-space
+    appgw-subnet-address-space       = var.appgw-subnet-address-space
+    gateway-subnet-address-space     = var.gateway-subnet-address-space
+    bastion-subnet-address-space     = var.bastion-subnet-address-space
+    mgmt-subnet-1-address-space      = var.mgmt-subnet-1-address-space
+    mgmt-subnet-2-address-space      = var.mgmt-subnet-2-address-space
+    shared-svcs-snet-address-space   = var.shared-svcs-snet-address-space
+    customer-name        = var.customer-name
+}
