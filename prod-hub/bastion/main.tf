16168a5f51fed656e7e93841df23a408ada8c435
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-16 23:44:33 +0300

    Update main.tf

--- prod-hub/bastion/main.tf
+++ prod-hub/bastion/main.tf
@@ -11,8 +11,13 @@ resource "azurerm_bastion_host" "bastion-svc" {
   name                = "bastion-${var.customerprefix}-${var.location}-01"
   location            = var.location
   resource_group_name = var.rg-name
-  sku                 = "Standard"
-
+  sku                 = "Premium"
+  copy_paste_enabled     = true
+  file_copy_enabled      = true
+  ip_connect_enabled     = false
+  shareable_link_enabled = false
+  tunneling_enabled      = true
+  zones:   ['1', '2', '3']
   ip_configuration {
     name                 = "configuration"
     subnet_id            = var.bastion-subnet-id
