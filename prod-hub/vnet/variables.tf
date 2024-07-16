beee75e2112046ec85ed993b1c66cdea943c9cff
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-16 22:36:03 +0300

    Update variables.tf

--- prod-hub/vnet/variables.tf
+++ prod-hub/vnet/variables.tf
@@ -17,3 +17,39 @@ variable regionprefix {
 variable location {
   type = string
 }
+
+variable "rg-name" {
+   default= "changeme"
+}
+
+variable "hub-vnet-address-space" {
+  default = "changeme"
+}
+
+variable "firewall-subnet-address-space" {
+  default = "changeme"
+}
+
+variable "appgw-subnet-address-space" {
+  default = "changeme"
+}
+
+variable "gateway-subnet-address-space" {
+  default = "changeme"
+}
+
+variable "bastion-subnet-address-space" {
+  default = "changeme"
+}
+
+variable "mgmt-subnet-1-address-space" {
+  default = "changeme"
+}
+
+variable "mgmt-subnet-2-address-space" {
+  default = "changeme"
+}
+
+variable "shared-svcs-snet-address-space" {
+  default = "changeme"
+}
