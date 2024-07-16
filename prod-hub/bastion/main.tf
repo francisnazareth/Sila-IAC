df5c3753b91ccd30e8de3da1c6d265c23ee49a66
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-16 23:19:02 +0300

    Update main.tf

--- prod-hub/bastion/main.tf
+++ prod-hub/bastion/main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_public_ip" "bastion-pip" {
   name                = "pip-bastion-${var.customerprefix}-${var.location}-01"
-  location            = var.rg-location
+  location            = var.location
   resource_group_name = var.rg-name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "bastion-svc" {
-  name                = "bastion-service"
-  location            = var.rg-location
+  name                = "bastion-${var.customerprefix}-${var.location}-01"
+  location            = var.location
   resource_group_name = var.rg-name
   sku                 = "Standard"
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = var.bastion-snet-id
+    subnet_id            = var.bastion-subnet-id
     public_ip_address_id = azurerm_public_ip.bastion-pip.id
   }
 }
