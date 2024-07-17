10ed724f587006dc2623783d1a937968c0bd4246
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-07-17 12:13:28 +0300

    Delete prod-hub directory

--- prod-hub/law/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_log_analytics_workspace" "law" {
-  location     = var.location
-  name = "law-${var.customerprefix}-${var.environment}-${var.regionprefix}-01"
-  tags     = var.tags
-  resource_group_name = var.law-rg-name
-  sku                 = "PerGB2018"
-}
